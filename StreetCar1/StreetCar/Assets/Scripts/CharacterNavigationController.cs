3c442e1a6edd1d871dc8cb00a09a019a05fe19e3
METOUR <dean94.elliott@gmail.com>
2019-09-11 18:06:38 -0400

    Initiail Commit

---
+++ StreetCar1/StreetCar/Assets/Scripts/CharacterNavigationController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class CharacterNavigationController : MonoBehaviour
+{
+
+    public bool reachedDestination = false;
+    private Transform waypoint;
+    public float movementSpeed = 1.0f;
+    public float rotationSpeed = 120f;
+    public float stopDistance = 0.2f;
+
+    Vector3 lastPositon;
+    Vector3 velocity;
+    Vector3 destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != destination)
+        {
+            Vector3 destinationDirection = destination - transform.position;
+            destinationDirection.y = 0;
+
+            float destinationDistance = destinationDirection.magnitude;
+
+            if (destinationDistance >= stopDistance)
+            {
+                reachedDestination = false;
+                Quaternion targetRotation = Quaternion.LookRotation(destinationDirection);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+            }
+            else
+            {
+                reachedDestination = true;
+            }
+
+            velocity = (transform.position - lastPositon) / Time.deltaTime;
+            velocity.y = 0;
+            var velocityMagnitude = velocity.magnitude;
+            velocity = velocity.normalized;
+            var fwdDotProduct = Vector3.Dot(transform.forward, velocity);
+            var rightDotProduct = Vector3.Dot(transform.right, velocity);
+        }
+    }
+
+
+    public void SetDestination(Vector3 destination)
+    {
+        this.destination = destination;
+        reachedDestination = false;
+    }
+}
---
+++ StreetCar1/StreetCar/Assets/Scripts/LightFade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFade : MonoBehaviour
+{
+
+    Light testLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(18f);
+            testLight.enabled = !testLight.enabled;
+        }
+    }
+
+}
