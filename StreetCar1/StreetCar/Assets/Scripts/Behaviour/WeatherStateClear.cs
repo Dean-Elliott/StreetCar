6fb838a6a6df1cacf9ba13efd3851255e34d0b44
Dean <dean94.elliott@gmail.com>
2019-09-13 20:12:23 -0400

    Skeleton for FSM

---
+++ StreetCar1/StreetCar/Assets/Scripts/Behaviour/WeatherStateClear.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviourMachine;
+
+public class WeatherStateClear : StateBehaviour
+{
+	// Called when the state is enabled
+	void OnEnable () {
+		Debug.Log("Started *State*");
+        SendEvent(1833751859);
+    }
+ 
+	// Called when the state is disabled
+	void OnDisable () {
+		Debug.Log("Stopped *State*");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
