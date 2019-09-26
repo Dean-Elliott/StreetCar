dc21746fe3f5af684688225adb1ff2a15d475b32
Dean <dean94.elliott@gmail.com>
2019-09-25 21:50:01 -0400

    Umbrellas!

---
+++ StreetCar1/StreetCar/Assets/Umbrella.cs
@@ -0,0 +1,17 @@
+﻿using DigitalRuby.RainMaker;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Umbrella : MonoBehaviour
+{
+    [SerializeField]
+    private Transform umbrella;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRaining = FindObjectOfType<RainScript>().RainFallParticleSystem.emission.enabled;
+        umbrella.gameObject.SetActive(isRaining);
+    }
+}
